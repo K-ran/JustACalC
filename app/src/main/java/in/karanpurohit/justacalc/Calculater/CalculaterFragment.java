3f8b84d3b7943d55a23071ab9293b031745dc982
Karan Purohit <Karnpurohit@gmail.com>
2016-06-22 17:56:11 +0530

    transfered the whole Calculater into a fragment

---
+++ app/src/main/java/in/karanpurohit/justacalc/Calculater/CalculaterFragment.java
@@ -0,0 +1,44 @@
+package in.karanpurohit.justacalc.Calculater;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import in.karanpurohit.justacalc.R;
+
+public class CalculaterFragment extends Fragment {
+
+
+    public CalculaterFragment () {
+        // Required empty public constructor
+    }
+
+    static TextView tvExpression,tvResult;
+    KeypadsPagerAdapter pagerAdapter;
+    ViewPager pager;
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate (R.layout.main_keypad_layout,container,false);
+        tvExpression = (TextView)view.findViewById (R.id.tvExpression);
+        tvResult = (TextView)view.findViewById (R.id.tvResult);
+        pager = (ViewPager)view.findViewById (R.id.vpSwipeKeyboard);
+        //--------------------------//
+
+        //Setting up view pager
+        pagerAdapter = new KeypadsPagerAdapter (getChildFragmentManager ());
+        pager.setAdapter (pagerAdapter);
+        pager.setCurrentItem (1);
+        //--------------------
+        return view;
+    }
+
+
+}
--- app/src/main/java/in/karanpurohit/justacalc/Calculater/MainActivity.java
+++ app/src/main/java/in/karanpurohit/justacalc/Calculater/MainActivity.java
@@ -2,9 +2,13 @@ package in.karanpurohit.justacalc.Calculater;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,42 +21,50 @@ import in.karanpurohit.justacalc.R;
 
 public class MainActivity extends AppCompatActivity implements NormalKeypadFragment.OnFragmentInteractionListener,
                                                                ScientificKeypadFragment.OnFragmentInteractionListener{
-
-    KeypadsPagerAdapter pagerAdapter;
-    ViewPager pager;
-    TextView tvExpression,tvResult;
+    FragmentManager fragmentManager ;
+    FragmentTransaction fragmentTransaction ;
     JsEvaluator jsEvaluator;
+
+
     ListView navigationList;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        // initilization starts here
+        fragmentManager = getSupportFragmentManager ();
+
+            if (savedInstanceState==null) {
+                fragmentTransaction = fragmentManager.beginTransaction ();
+                fragmentTransaction.add (R.id.content_frame,new CalculaterFragment ());
+                fragmentTransaction.commit ();
+            }
         jsEvaluator = new JsEvaluator (this);
         navigationList = (ListView)findViewById (R.id.lvNavigationList);
         navigationList.setAdapter (new DrawerListAdapter (this));
-        // initilization starts here
-        pager = (ViewPager)findViewById (R.id.vpSwipeKeyboard);
-        tvExpression = (TextView)findViewById (R.id.tvExpression);
-        tvResult = (TextView)findViewById (R.id.tvResult);
-        //--------------------------//
-
-        //Setting up view pager
-        pagerAdapter = new KeypadsPagerAdapter (getSupportFragmentManager());
-        pager.setAdapter (pagerAdapter);
-        pager.setCurrentItem (1);
-        //--------------------
+        drawerLayout = (DrawerLayout)findViewById (R.id.drawer_layout);
 
         // TODO:  Hiding the soft keyboard if using edittext
         //-----------------
 
 
+        //Handling navigation drawer click listener
+        navigationList.setOnItemClickListener (new AdapterView.OnItemClickListener () {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                drawerLayout.closeDrawers ();
+            }
+        });
+
     }
+
 
 
     public void buttonClicked(View view){
         TextView tvButton = (TextView)view;
         switch(tvButton.getId ()){
-            case R.id.tvKBMul:tvExpression.setText (tvExpression.getText ().toString ()+"*");break;
+            case R.id.tvKBMul:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString () + "*");break;
             case R.id.tvKB0:
             case R.id.tvKB1:
             case R.id.tvKB2:
@@ -70,37 +82,39 @@ public class MainActivity extends AppCompatActivity implements NormalKeypadFragm
             case R.id.tvKBRightBracket:
             case R.id.tvKBComma:
             case R.id.tvKBDot:
-                tvExpression.setText (tvExpression.getText ().toString () + tvButton.getText ());
+                CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString () + tvButton.getText ());
                 ;break;
             case R.id.tvKBEqual:
-                jsEvaluator.evaluate (tvExpression.getText ().toString (), new JsCallback () {
+                jsEvaluator.evaluate (CalculaterFragment.tvExpression.getText ().toString (), new JsCallback () {
                     @Override
                     public void onResult (final String result) {
 
-                        tvResult.setText (result);
+                        CalculaterFragment.tvResult.setText (result);
                     }
                 });
                 break;
-            case R.id.tvKBDel: tvExpression.setText ("");break;
-            case R.id.tvKBPi:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.PI);break;
-            case R.id.tvKBexponent:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.E);break;
-            case R.id.tvKBPower:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.POWER);break;
-            case R.id.tvKBln:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.LN);break;
-            case R.id.tvKBlog:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.LOG);break;
-            case R.id.tvKBSqrt:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.SQRT);break;
-            case R.id.tvKBSin:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.SINE);break;
-            case R.id.tvKBCos:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.COS);break;
-            case R.id.tvKBTan:tvExpression.setText (tvExpression.getText ().toString ()+CONSTANTS.TAN);break;
+            case R.id.tvKBDel: CalculaterFragment.tvExpression.setText ("");break;
+            case R.id.tvKBPi:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.PI);break;
+            case R.id.tvKBexponent:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.E);break;
+            case R.id.tvKBPower:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.POWER);break;
+            case R.id.tvKBln:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.LN);break;
+            case R.id.tvKBlog:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.LOG);break;
+            case R.id.tvKBSqrt:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.SQRT);break;
+            case R.id.tvKBSin:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.SINE);break;
+            case R.id.tvKBCos:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.COS);break;
+            case R.id.tvKBTan:CalculaterFragment.tvExpression.setText (CalculaterFragment.tvExpression.getText ().toString ()+CONSTANTS.TAN);break;
         }
 
-        jsEvaluator.evaluate (tvExpression.getText ().toString (), new JsCallback () {
+        jsEvaluator.evaluate (CalculaterFragment.tvExpression.getText ().toString (), new JsCallback () {
             @Override
             public void onResult (final String result) {
 
-                tvResult.setText (result);
+                CalculaterFragment.tvResult.setText (result);
             }
         });
     }
+
+
 
 
     @Override
