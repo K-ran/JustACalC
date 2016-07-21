f0f2003008ff252785e70736f4e7578491a670c8
Karn Purohit <karnpurohit@gmail.com>
2016-07-21 19:37:32 +0530

    created registration

--- app/src/main/java/in/karanpurohit/justacalc/SignInUp/SigninActivity.java
+++ app/src/main/java/in/karanpurohit/justacalc/SignInUp/SigninActivity.java
@@ -2,6 +2,7 @@ package in.karanpurohit.justacalc.SignInUp;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,30 +26,39 @@ import in.karanpurohit.justacalc.R;
 
 public class SigninActivity extends AppCompatActivity implements PostRequestHandler.ResponseHandler {
 
+    public static int SIGN_UP_REQUEST_CODE=0;
     Button signin;
     EditText etEmail,etPassword;
     ProgressDialog progress;
     JSONObject userData;
+    TextView signupLink;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_signin);
+        setContentView(R.layout.activity_signin);
         signin = (Button)findViewById (R.id.btnActualSignin);
         etEmail = ((EditText)findViewById (R.id.etSigninEmail));
         etPassword = ((EditText)findViewById (R.id.etSiginPassword));
         progress = new ProgressDialog(this);
         progress.setMessage ("Signing You in ..");
-
+        signupLink = (TextView)findViewById(R.id.tvSignupLink);
         etEmail.setImeActionLabel ("", EditorInfo.IME_ACTION_NEXT);
 
-        signin.setOnClickListener (new View.OnClickListener () {
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
-                if(!(isEmailValid ()&&isPasswordValid ()))
+            public void onClick(View v) {
+                if (!(isEmailValid() && isPasswordValid()))
                     return;
                 //TODO: Start the login process
-                progress.show ();
+                progress.show();
                 sendRequest();
+            }
+        });
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivityForResult(intent, SIGN_UP_REQUEST_CODE);
             }
         });
     }
@@ -64,7 +76,7 @@ public class SigninActivity extends AppCompatActivity implements PostRequestHand
         String password = etPassword.getText ().toString ();
         if(!password.matches ("[A-Za-z0-9]+")){
             if(password.equals (""))
-                etEmail.setError ("Required field");
+                etPassword.setError ("Required field");
             else
                 etPassword.setError ("Password should be alpha numeric value");
             return false;
@@ -105,6 +117,13 @@ public class SigninActivity extends AppCompatActivity implements PostRequestHand
             MyToast.CREATE (getApplicationContext (), "Incorrect Credentials", MyToast.FAIL).show ();
         else
             MyToast.CREATE (getApplicationContext (), "Something went wrong", MyToast.FAIL).show ();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {;
+        if(resultCode == RESULT_OK && requestCode==SIGN_UP_REQUEST_CODE) {
+            MyToast.CREATE(this,"Account created, please ligin",MyToast.SUCCESS).show();
+        }
     }
 }
 
